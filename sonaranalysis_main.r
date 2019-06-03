56b19e1d9b216c439c74142ea9bb20d84ebd8646
nilsolav <nils.olav.handegard@imr.no>
2019-06-03 11:25:56 +0200

    Create sonaranalysis_main.r

---
+++ sonaranalysis_main.r
@@ -0,0 +1,12 @@
+% This script is the master script for analysing the sonar data for the sonar taskin REDUS WP2
+
+% sonar analysis script
+D <- '\\ces.imr.no\mea\2017_14809_REDUS\cruise_data'
+years <- c(2015,2016,2018,2019)
+sonars <- c('SU90','MS70')
+
+% Test parse data
+
+% Create StoX projects
+
+
